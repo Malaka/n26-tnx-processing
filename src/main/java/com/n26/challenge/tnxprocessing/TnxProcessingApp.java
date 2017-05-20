b0791b587ab45de166bd726004a91c8aefa28026
malaka <malaka@proavos.com>
2017-05-20 02:35:28 +0100

    init project with spring boot

---
+++ src/main/java/com/n26/challenge/tnxprocessing/TnxProcessingApp.java
@@ -0,0 +1,12 @@
+package com.n26.challenge.tnxprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnxProcessingApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnxProcessingApp.class, args);
+	}
+}
