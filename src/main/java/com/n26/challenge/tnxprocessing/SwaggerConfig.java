fb9d041494155002243e8021258b3725e52ee499
malaka <malaka@proavos.com>
2017-05-20 14:10:07 +0100

    Swagger detail fix

--- src/main/java/com/n26/challenge/tnxprocessing/SwaggerConfig.java
+++ src/main/java/com/n26/challenge/tnxprocessing/SwaggerConfig.java
@@ -4,6 +4,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,9 +34,9 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-			.title("Transaction stat aggregator")
-			.description("Transaction stat aggregator REST api")
-			.contact("Malaka Ekanayake")
+			.title("Transaction Statistics Aggregator")
+			.description("Transaction Statistics aggregator REST api")
+			.contact(new Contact("Malaka Ekanayake", "", "malaka.ekanayake@gmail.com"))
 			.version("1.0")
 			.build();
 	}
